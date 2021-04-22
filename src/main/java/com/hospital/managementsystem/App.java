e98df15d3cf9a16388fec63842da2f93dedaf7cc
lenovo <WangNingJava@163.com>
2021-04-22 13:48:41 +0800

    [init]

---
+++ src/main/java/com/hospital/managementsystem/App.java
@@ -0,0 +1,15 @@
+package com.hospital.managementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ning.wang
+ * @date 2021/4/22 13:46
+ */
+@SpringBootApplication
+public class App {
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+}
---
+++ src/main/java/com/hospital/managementsystem/domin/Test.java
@@ -0,0 +1,8 @@
+package com.hospital.managementsystem.domin;
+
+/**
+ * @author ning.wang
+ * @date 2021/4/22 13:39
+ */
+public class Test {
+}
